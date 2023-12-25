5cade0303ccdd2ecdeefa0cd153fe1ad39e277d9
anuidev8 <angelarrieta34@gmail.com>
2023-12-25 11:41:42 -0500

    updated: updated api contacts

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 const { mongoUri } = require('./vars')
-const Log = require('./../utils/loggerService')
 console.log(mongoUri)
 if(process.env.NODE_ENV == 'development') {
     mongoose.set('debug', true)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 const app = require('./configs/server')
 const { port } = require('./configs/vars')
-const Log = require('./utils/loggerService')
+
 
 require('./configs/database')
 
-app.listen(port, () => Log.info(`Server running on port ${port}`))
+app.listen(port, () => console.log(`Server running on port ${port}`))
