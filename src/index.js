92ef6c4dedfa2f1d8bcaa77d09b471b57089dad9
anuidev8 <angelarrieta34@gmail.com>
2023-12-26 11:14:29 -0500

    updated cors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ const app = require('./configs/server')
 const { port } = require('./configs/vars')
 const cors = require('cors')
 require('./configs/database')
-app.use(cors());
+
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
