8a7ee630836d0c99a7ead1bce5cd7b4d76cac8e3
anuidev8 <angelarrieta34@gmail.com>
2023-12-26 08:53:05 -0500

    updated cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 const app = require('./configs/server')
 const { port } = require('./configs/vars')
-
+const cors = require('cors')
 require('./configs/database')
+app.use(cors());
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
