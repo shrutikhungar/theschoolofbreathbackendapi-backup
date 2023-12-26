0e5a3bebd8d53c015d3a47d744ad523f0204e9f7
anuidev8 <angelarrieta34@gmail.com>
2023-12-26 11:37:05 -0500

    updated db conected

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose')
 const { mongoUri } = require('./vars')
-console.log(mongoUri)
+
 if(process.env.NODE_ENV == 'development') {
     mongoose.set('debug', true)
 }
 
 
-mongoose.connect('mongodb+srv://angelarrieta34:parzival-13@cluster0.mykndeo.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(mongoUri, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000 
 })
     .then(db => console.log('Database is connected'))
     .catch(err => { console.error(err), console.log(err) })
