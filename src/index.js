d10ca0e32fa9a84f7c8601fce9cc4cda42def830
anuidev8 <angelarrieta34@gmail.com>
2024-01-22 22:08:51 -0500

    updated index app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@ require('dotenv').config()
 const app = require('./configs/server')
 const { port } = require('./configs/vars')
 require('./configs/database')
-
-
 app.listen(port, () => console.log(`Server running on port ${port}`))
