30793afb93dfe952cfd376258880e4ef134dfb90
anuidev8 <angelarrieta34@gmail.com>
2024-02-05 19:12:51 -0500

    updated upload files functions

--- src/controllers/upload.controller.js
+++ src/controllers/upload.controller.js
@@ -1,190 +1,172 @@
 const multer = require('multer');
-const {GridFsStorage} = require('multer-gridfs-storage');
-
-const Music = require("../models/music.model");
-const { mongoUri } = require('../configs/vars');
-const crypto = require('crypto');
-const path = require('path');
-
-
-
+const upload = multer({ storage: multer.memoryStorage() });
+const { Storage } = require('@google-cloud/storage');
 const mongoose = require('mongoose');
-const Grid = require('gridfs-stream');
-const { log } = require('console');
+const Music = require("../models/music.model");
+const fs = require('fs');
+// Decode the environment variable to a temporary file
 
-// Ensure mongoose connection is open
-const conn = mongoose.createConnection(mongoUri);
-
-let gfsAudio, gfsImages;
-conn.once('open', () => {
-    console.log('Database is connected');
-    // Initialize GridFS Bucket for audio
-    gfsAudio = new mongoose.mongo.GridFSBucket(conn.db, {
-        bucketName: 'audioUploads'
-    });
-    // Initialize GridFS Bucket for images
-    gfsImages = new mongoose.mongo.GridFSBucket(conn.db, {
-        bucketName: 'imageUploads'
-    });
+// Initialize Google Cloud Storage
+const storage = new Storage({
+  credentials: JSON.parse(Buffer.from(process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64, 'base64').toString('ascii'))
 });
-// Create storage engine
-const storage = new GridFsStorage({
-    url: mongoUri,
-    file: (req, file) => {
-      return new Promise((resolve, reject) => {
-        crypto.randomBytes(16, (err, buf) => {
-          if (err) {
-            return reject(err);
-          }
-          const filename = buf.toString('hex') + path.extname(file.originalname);
-          const fileInfo = {
-            filename: filename,
-            bucketName: file.mimetype.startsWith('audio') ? 'audioUploads' : 'imageUploads'
-          };
-          resolve(fileInfo);
-        });
-      });
-    }
-  });
-  exports.upload = multer({ storage });
 
 
-  exports.uploadFile = async (req, res, next) => {
-    try {
-     
-  const audioFile = req.files['audioFile'][0];
-  const imageFile = req.files['imageFile'][0];
+const bucketName = 'sleepmusic';
 
-  // Create a new music document
-  const newMusic = new Music({
-    name: req.body.name,
-    imageFilename:imageFile.filename,
-    audioFilename: audioFile.filename,
-    categories:req.body.categoryId,
-    description:req.body.description
+const uploadFileToStorage = async (file, isPoster = false) => {
+  // Determine the file path based on whether it's a poster
+  const filePath = isPoster ? `images/${file.originalname}` : file.originalname;
+
+  const blob = storage.bucket(bucketName).file(filePath);
+  const blobStream = blob.createWriteStream();
+
+  await new Promise((resolve, reject) => {
+      blobStream.on('error', reject);
+      blobStream.on('finish', resolve);
+      blobStream.end(file.buffer);
   });
 
-  newMusic.save((err, music) => {
-    if (err) return res.status(500).send(err);
-    res.status(201).send({ music: music, message: 'Audio and image files uploaded successfully' });
-  });
-    } catch (error) {
-      next(error);
-    }
-  };
-
-  exports.deleteMusicFile = async (req, res) => {
-    try {
-      // Find the music item by id
-      const music = await Music.findById(req.params.musicId);
-      if (!music) {
-          return res.status(404).json({ message: 'Music item not found' });
-      }
-  
-      // Function to delete file from GridFS
-      const deleteFileFromGridFS = async (gfs, filename) => {
-          const files = await gfs.find({ filename: filename }).toArray();
-          if (files.length > 0) {
-              const fileId = files[0]._id;
-              await gfs.delete(fileId);
-          }
-      };
-  
-      // Delete the audio file
-      if (music.audioFilename) {
-          await deleteFileFromGridFS(gfsAudio, music.audioFilename);
-      }
-  
-      // Delete the image file, if it exists
-      if (music.imageFilename) {
-          await deleteFileFromGridFS(gfsImages, music.imageFilename);
-      }
-  
-      // Delete the music item from the collection
-      await Music.findByIdAndRemove(req.params.musicId);
-  
-      res.status(200).json({ message: 'Music item and associated files deleted successfully' });
-  } catch (err) {
-      console.error("Error occurred:", err);
-      res.status(500).json({ message: err.message });
-  }
-  }
-
-  // Endpoint to stream audio file
-exports.streamAudio =async (req, res) => {
-  const filename = req.params.filename;
-    // Determine the bucket based on file type or other criteria
-    const bucket = filename.endsWith('.mp3') ? gfsAudio : gfsImages;
-
-    if (!bucket) {
-        return res.status(500).send('GridFS bucket not initialized');
-    }
-
-    bucket.find({ filename: filename }).toArray((err, files) => {
-        if (err || !files || files.length === 0) {
-            return res.status(404).send('File not found');
-        }
-
-        const file = files[0];
-        if (file.contentType.startsWith('audio') || file.contentType.startsWith('image')) {
-            const readstream = bucket.openDownloadStream(file._id);
-            res.set('Content-Type', file.contentType);
-            readstream.pipe(res);
-        } else {
-            res.status(404).send('Unsupported file type');
-        }
-    });
+  return `https://storage.googleapis.com/${bucketName}/${blob.name}`;
 };
 
-exports.editMusic = async (req, res, next) =>{
+async function deleteFileFromStorage(fileName, isPoster = false) {
+  // Determine the file path based on whether it's a poster
+  const filePath = isPoster ? `images/${fileName}` : fileName;
+
+  await storage.bucket(bucketName).file(filePath).delete();
+}
+
+exports.uploadFiles = async (req, res, next) => {
+    try {
+        // Ensure both sound and poster files are uploaded
+        if (!req.files.audioFile || !req.files.imageFile) {
+            return res.status(400).send({message:'Both sound and poster files are required.'});
+        }
+
+        const audioFile = req.files.audioFile[0]; // Assuming `audioFile` is the field name for the sound file
+        const imageFile = req.files.imageFile[0]; // Assuming `imageFile` is the field name for the poster file
+
+        // Function to upload file to Google Cloud Storage and return the URL
+        const uploadFileToStorage = async (file, isPoster = false) => {
+          // Determine the file path based on whether it's a poster
+          const filePath = isPoster ? `images/${file.originalname}` : file.originalname;
+      
+          const blob = storage.bucket(bucketName).file(filePath);
+          const blobStream = blob.createWriteStream();
+      
+          await new Promise((resolve, reject) => {
+              blobStream.on('error', reject);
+              blobStream.on('finish', resolve);
+              blobStream.end(file.buffer);
+          });
+      
+          return `https://storage.googleapis.com/${bucketName}/${blob.name}`;
+      };
+      
+
+        // Upload files
+        const audioFileUrl = await uploadFileToStorage(audioFile);
+        const imageFileUrl = await uploadFileToStorage(imageFile,true);
+
+        // Save music details in MongoDB
+        const music = new Music({
+            // Add other music details here, e.g., title, artist
+            name: req.body.name,
+            categories:req.body.categoryId,
+            description:req.body.description,
+            audioFilename: audioFileUrl,
+            imageFilename: imageFileUrl,
+        });
+
+        await music.save();
+
+        res.status(200).send({
+            message: 'Music uploaded and saved successfully.',
+            data: {
+                soundUrl: audioFileUrl,
+                posterUrl: imageFileUrl,
+                // Include any other details you saved
+            }
+        });
+    } catch (error) {
+        return res.status(500).send({ message: error.message });
+    }
+};
+
+exports.editMusicItem = async (req, res) => {
   try {
-    const musicId = req.params.musicId;
-    const currentMusic = await Music.findById(musicId);
-    const updateData = {
+      const { musicId } = req.params; // Music item's ID from URL parameter
+      const musicItem = await Music.findById(musicId);
+      const updateData = {
         name: req.body.name,
         categories: req.body.categoryId,
         description: req.body.description,
         // Add other fields as necessary
     };
-     // Function to delete file from GridFS
-     const deleteFileFromGridFS = async (gfs, filename) => {
-      const files = await gfs.find({ filename: filename }).toArray();
-      if (files.length > 0) {
-          const fileId = files[0]._id;
-          await gfs.delete(fileId);
+      if (!musicItem) {
+          return res.status(404).send({message: 'Music item not found.'});
       }
-  };
 
-  
-    // Handle file updates (if files are part of the update)
-    if (req.files) {
-        const audioFile = req.files['audioFile'] ? req.files['audioFile'][0] : null;
-        const imageFile = req.files['imageFile'] ? req.files['imageFile'][0] : null;
+      // Check for new file uploads and delete old files if new ones are provided
+      if (req.files && req.files.audioFile) {
+          if (musicItem.audioFilename) {
+              const oldaudioFileName = musicItem.audioFilename.split('/').pop();
+              await deleteFileFromStorage(oldaudioFileName);
+          }
+          const newaudioFile = req.files.audioFile[0];
+          updateData.audioFilename = await uploadFileToStorage(newaudioFile); // Update with new sound URL
+      }
 
-        if (audioFile) {
-          if (currentMusic && currentMusic.audioFilename) {
-            // Delete the old audio file from GridFS
-            await deleteFileFromGridFS(gfsAudio, currentMusic.audioFilename);
-        }
-    
-        // Update with the new audio file's filename
-        updateData.audioFilename = audioFile.filename;
+      if (req.files && req.files.imageFile) {
+          if (musicItem.imageFilename) {
+              const oldimageFileName = musicItem.imageFilename.split('/').pop();
+              await deleteFileFromStorage(oldimageFileName,true);
+          }
+          const newimageFile = req.files.imageFile[0];
+          updateData.imageFilename = await uploadFileToStorage(newimageFile, true); // Update with new poster URL
+      }
+
+      // Save the updated music item in the database
+      const updatedMusic = await Music.findByIdAndUpdate(musicId, updateData, { new: true });
+
+      res.status(200).send({
+          message: 'Music item updated successfully.',
+          data: updatedMusic,
+      });
+  } catch (error) {
+      res.status(500).send({ message: error.message });
+  }
+};
+
+exports.deleteMusicItem = async (req, res) =>{
+  const { musicId } = req.params;
+
+    try {
+        const musicItem = await Music.findById(musicId);
+      
+
+        if (!musicItem) {
+            return res.status(404).send({ message: 'Music item not found.' });
         }
 
-        if (imageFile) {
-          if (currentMusic && currentMusic.imageFilename) {
-            // Delete the old audio file from GridFS
-           await deleteFileFromGridFS(gfsAudio, currentMusic.imageFilename);
+        // Delete sound file if it exists
+        if (musicItem.audioFilename) {
+            const audioFileName = musicItem.audioFilename.split('/').pop();
+            await deleteFileFromStorage(audioFileName); // Assuming this is not in the `images` folder
         }
-            updateData.imageFilename = imageFile.filename;
-            // Delete the old image file from GridFS
+
+        // Delete poster file if it exists
+        if (musicItem.imageFilename) {
+            const imageFileName = musicItem.imageFilename.split('/').pop();
+            await deleteFileFromStorage(imageFileName, true); // Assuming this is in the `images` folder
         }
+
+        // Delete the music item from the database
+        await Music.deleteOne({ _id: musicId });
+
+        res.status(200).send({ message: 'Music item and related files deleted successfully.' });
+    } catch (error) {
+        res.status(500).send({ message: error.message });
     }
-
-    const updatedMusic = await Music.findByIdAndUpdate(musicId, updateData, { new: true });
-    res.status(200).json({ message: 'Music updated successfully', music: updatedMusic });
-} catch (error) {
-    next(error)
 }
-}
-  
--- src/routes/upload.routes.js
+++ src/routes/upload.routes.js
@@ -1,12 +1,12 @@
 const { Router } = require("express");
 const controller = require("../controllers/upload.controller");
-
+const multer = require('multer');
 let router = Router();
+// Initialize multer for file handling
+const uploadMulter = multer({storage: multer.memoryStorage()});
 
-
-router.route("/add").post(controller.upload.fields([{ name: 'audioFile', maxCount: 1 }, { name: 'imageFile', maxCount: 1 }]),controller.uploadFile) 
-router.route("/edit/:musicId").put(controller.upload.fields([{ name: 'audioFile', maxCount: 1 }, { name: 'imageFile', maxCount: 1 }]),controller.editMusic) 
-router.route("/delete/:musicId").delete(controller.deleteMusicFile) 
-router.get('/file/:filename', controller.streamAudio);
+router.route("/add").post(uploadMulter.fields([{ name: 'audioFile' }, { name: 'imageFile' }]),controller.uploadFiles) 
+router.route("/edit/:musicId").put(uploadMulter.fields([{ name: 'audioFile' }, { name: 'imageFile' }]),controller.editMusicItem) 
+router.route("/delete/:musicId").delete(controller.deleteMusicItem) 
 
 module.exports = router;
