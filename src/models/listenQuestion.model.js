564c86511ded78eed41d50e41171ce849ccbedc2
anuidev8 <angelarrieta34@gmail.com>
2025-04-29 00:11:06 -0500

    it has been updated user email

--- src/models/listenQuestion.model.js
+++ src/models/listenQuestion.model.js
@@ -2,7 +2,7 @@ const {Schema} = require('mongoose');
 const mongoose = require('mongoose');
 
 const  listenQuestionSchema = new Schema({
-    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    userId: { type: String, required: true },
     faqId: { type: Schema.Types.ObjectId, ref: 'FAQ', required: true },
     count: { type: Number, default: 1 },
 });
